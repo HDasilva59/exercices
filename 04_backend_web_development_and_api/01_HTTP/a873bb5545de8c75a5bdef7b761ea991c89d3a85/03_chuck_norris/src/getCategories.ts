b71cb7e0bf25dc43c6d931f50fb2397c8becc639
Da silva <dasilva.hugo59200@gmail.com>
2022-02-14 11:26:11 +0100

    add part 3 day 1 3rd exercise

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getCategories.ts
@@ -1,7 +1,15 @@
 import request from "@fewlines-education/request";
 
-function getCategories() {
-  // code the function here
+function getCategories(): void {
+  request("https://api.chucknorris.io/jokes/categories", (error, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    } else {
+      const data = JSON.parse(body);
+      console.log(data);
+    }
+  });
 }
 
 // leave line below for tests to work properly
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/03_chuck_norris/src/getChuckNorrisJoke.ts
@@ -1,7 +1,15 @@
 import request from "@fewlines-education/request";
 
-function getChuckNorrisJoke() {
-  // code the function here
+function getChuckNorrisJoke(category: string): void {
+  request(`https://api.chucknorris.io/jokes/random?category=${category}`, (error, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    } else {
+      const dajoke = JSON.parse(body);
+      console.log(dajoke);
+    }
+  });
 }
 
 // leave line below for tests to work properly
