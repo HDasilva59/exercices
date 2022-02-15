c56067dabcc3effd248d1f9b255f6e2ec6d3b07e
Da silva <dasilva.hugo59200@gmail.com>
2022-02-15 09:52:14 +0100

    add day 2 part 3 warmup exercises

--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/01_warmup_filtering_by_first_letter/src/filter_by_first_letter.ts
@@ -1,5 +1,9 @@
 import { Customer } from "../data/customers";
 
-export function filterByFirstLetter() {
-  // Code here
+export function filterByFirstLetter(customers:Customer[], letter:string):Customer[] {
+  const filteredCustomers:Customer[] = []
+  customers.forEach(person => {if (person.last_name.indexOf(letter) === 0){
+    filteredCustomers.push(person)
+  }})
+  return filteredCustomers
 }
--- 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
+++ 04_backend_web_development_and_api/02_APIs/0570c0a978c86da20a524b74e3f45e43f70842eb/02_warmum_filter_by_first_letter_alphabetically/src/index.ts
@@ -1,1 +1,17 @@
-// code here
+import { Customer } from "../data/customers";
+
+export function filterByFirstLetterOrdered(customers:Customer[], letter:string):Customer[] {
+  const filteredCustomers:Customer[] = []
+  customers.forEach(person => {if (person.last_name.indexOf(letter) === 0){
+    filteredCustomers.push(person)
+  }})
+  filteredCustomers.sort((a, b) => {
+    if (a.last_name < b.last_name){
+      return -1;
+    }
+    if(a.last_name > b.last_name){
+      return 1;
+    }
+  return 0;})
+  return filteredCustomers
+}
